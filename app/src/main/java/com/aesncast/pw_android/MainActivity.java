567d3172c6ac457efd7d58b5439e784a4f56629b
aesncast <94376350+aesncast@users.noreply.github.com>
2021-12-02 10:44:38 -0500

    Add hamburger menu and settings fragment

--- app/src/main/java/com/aesncast/pw_android/MainActivity.java
+++ app/src/main/java/com/aesncast/pw_android/MainActivity.java
@@ -1,94 +1,157 @@
 package com.aesncast.pw_android;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.lifecycle.Lifecycle;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MenuItem;
 
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements TabLayoutMediator.TabConfigurationStrategy {
+public class MainActivity extends AppCompatActivity {
+    public static Handler handler;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
 
-    private TabLayout tabLayout;
-    private ViewPager2 viewPager;
+    private ActionBarDrawerToggle drawerToggle;
 
-    private RecentPasswordsFragment recentPasswordsFragment;
-    private DomainsUsersFragment domainsUsersFragment;
-    private SequencesFragment sequencesFragment;
-    private List<String> titles;
+    private MainTabsFragment mainFragment;
+    private SettingsFragment settingsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tabLayout = findViewById(R.id.main_tab_layout);
-        viewPager = findViewById(R.id.main_view_pager);
+        handler = new Handler();
 
-        setViewPagerAdapter();
-        new TabLayoutMediator(tabLayout, viewPager, this).attach();
+        setupFragments();
+        setupTopbar();
+        setupDrawer();
+        setupDrawerToggle();
+
+        setActiveFragment(mainFragment);
+        setTitle("Home");
     }
 
-    public void setViewPagerAdapter()
+    private void setupFragments()
     {
-        ViewPagerFragmentAdapter a = new ViewPagerFragmentAdapter(getSupportFragmentManager(), getLifecycle());
-        titles = new ArrayList<>();
+        this.mainFragment = new MainTabsFragment();
+        this.settingsFragment = new SettingsFragment();
+    }
 
-        titles.add(getString(R.string.recent));
-        recentPasswordsFragment = new RecentPasswordsFragment();
+    private void setupTopbar()
+    {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        titles.add(getString(R.string.domains_and_users));
-        domainsUsersFragment = new DomainsUsersFragment();
+        // This will display an Up icon (<-), we will replace it with hamburger later
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        titles.add(getString(R.string.sequences));
-        sequencesFragment = new SequencesFragment();
+        // Find our drawer view
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
 
-        a.addFragment(recentPasswordsFragment);
-        a.addFragment(domainsUsersFragment);
-        a.addFragment(sequencesFragment);
+    private void setupDrawerToggle() {
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open,  R.string.close);
+        drawerToggle.setDrawerIndicatorEnabled(true);
+        drawerToggle.syncState();
 
-        viewPager.setAdapter(a);
+        drawerLayout.addDrawerListener(drawerToggle);
     }
 
     @Override
-    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-        tab.setText(titles.get(position));
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
     }
 
-    public static class ViewPagerFragmentAdapter extends FragmentStateAdapter {
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
 
-        private List<Fragment> fragments = new ArrayList<>();
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
 
-        public ViewPagerFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
-            super(fragmentManager, lifecycle);
+        /*
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
         }
+         */
 
-        public void setFragments(List<Fragment> fragments) {
-            this.fragments = fragments;
-        }
+        return super.onOptionsItemSelected(item);
+    }
 
-        public void addFragment(Fragment fragment) {
-            fragments.add(fragment);
-        }
+    private void setupDrawer()
+    {
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
 
-        @Override
-        public int getItemCount() {
-            return fragments.size();
-        }
+        setupDrawerContent(navigationView);
+    }
 
-        @NonNull
-        @Override
-        public Fragment createFragment(int position) {
-            return fragments.get(position);
-        }
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+            menuItem -> {
+                selectDrawerItem(menuItem);
+                return true;
+            });
+    }
+
+    private void setActiveFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        handler.post(() -> fragmentManager.beginTransaction().replace(R.id.main_content_frame, fragment).commit());
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Insert the fragment by replacing any existing fragment
+        Fragment fragment = null;
+
+        int itemId = menuItem.getItemId();
+        if (itemId == R.id.nav_home)
+            fragment = mainFragment;
+        else if (itemId == R.id.nav_settings)
+            fragment = settingsFragment;
+
+        setActiveFragment(fragment);
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+
+        drawerLayout.closeDrawers();
     }
 }
---
+++ app/src/main/java/com/aesncast/pw_android/MainTabsFragment.java
@@ -0,0 +1,125 @@
+package com.aesncast.pw_android;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Parcelable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link MainTabsFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class MainTabsFragment extends Fragment implements TabLayoutMediator.TabConfigurationStrategy {
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager;
+
+    private RecentPasswordsFragment recentPasswordsFragment;
+    private DomainsUsersFragment domainsUsersFragment;
+    private SequencesFragment sequencesFragment;
+    private List<String> titles;
+
+    public MainTabsFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainTabsFragment newInstance() {
+        MainTabsFragment fragment = new MainTabsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_main_tabs, container, false);
+        setupTabs(v);
+        return v;
+    }
+
+    private void setupTabs(View v)
+    {
+        tabLayout = v.findViewById(R.id.main_tab_layout);
+        viewPager = v.findViewById(R.id.main_view_pager);
+        viewPager.setSaveEnabled(false);
+
+        setViewPagerAdapter();
+        new TabLayoutMediator(tabLayout, viewPager, this).attach();
+    }
+
+    private void setViewPagerAdapter()
+    {
+        ViewPagerFragmentAdapter a = new ViewPagerFragmentAdapter(getChildFragmentManager(), getLifecycle());
+        titles = new ArrayList<>();
+
+        titles.add(getString(R.string.recent));
+        recentPasswordsFragment = new RecentPasswordsFragment();
+
+        titles.add(getString(R.string.domains_and_users));
+        domainsUsersFragment = new DomainsUsersFragment();
+
+        titles.add(getString(R.string.sequences));
+        sequencesFragment = new SequencesFragment();
+
+        a.addFragment(recentPasswordsFragment);
+        a.addFragment(domainsUsersFragment);
+        a.addFragment(sequencesFragment);
+
+        viewPager.setAdapter(a);
+    }
+
+    @Override
+    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+        tab.setText(titles.get(position));
+    }
+
+    public static class ViewPagerFragmentAdapter extends FragmentStateAdapter {
+
+        private List<Fragment> fragments = new ArrayList<>();
+
+        public ViewPagerFragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+            super(fragmentManager, lifecycle);
+        }
+
+        public void setFragments(List<Fragment> fragments) {
+            this.fragments = fragments;
+        }
+
+        public void addFragment(Fragment fragment) {
+            fragments.add(fragment);
+        }
+
+        @Override
+        public int getItemCount() {
+            return fragments.size();
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            return fragments.get(position);
+        }
+    }
+}
